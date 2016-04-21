6fcca0a8cbe868ebdf75acca2cb51272ac95e7a6
Claire Hu <hum085@newschool.edu>
2016-04-21 17:27:49 -0400

    npc triggered dialogue & assets clean-up

---
+++ Assets/Scripts/playerControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playerControl : MonoBehaviour {
+
+	float playerSpeed = 5.0f;
+
+	public TextAsset textFiles;
+	public string[] textLines;
+
+	public bool canMove = true;
+
+	void Start(){
+		
+		if (textFiles != null) {
+			textLines = (textFiles.text.Split());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if (canMove) {
+			
+			if (Input.GetAxis ("Horizontal") != 0) {
+				//go left
+				if (Input.GetAxisRaw ("Horizontal") < 0) {
+
+					transform.position -= new Vector3 (playerSpeed * Time.deltaTime, 0, 0);
+				//go right
+				} else if (Input.GetAxisRaw ("Horizontal") > 0) {
+					
+					transform.position += new Vector3 (playerSpeed * Time.deltaTime, 0, 0);
+				}
+
+			}
+		//}
+
+	
+	}
+}
---
+++ Assets/Scripts/textBoxManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+
+public class textBoxManager : MonoBehaviour {
+
+	public GameObject textBox;
+	public Text theText;
+
+	public TextAsset textFiles;
+	public string[] textLines;
+
+	public int currentLine;
+	public int endAtLine;
+
+//	public PlayerController player;
+	GameObject player;
+
+	public bool isActive = false;
+	public bool stopPlayerMovement = false;
+
+	void Start(){
+
+//		player = GameObject.FindObjectOfType<playerControl>();
+		player = GameObject.Find("Player");
+
+		if (textFiles != null) {
+			textLines = (textFiles.text.Split('\n'));
+		}
+
+		if (endAtLine == 0) {
+			endAtLine = textLines.Length - 1;
+		}
+
+		if (isActive) {
+			EnableTextBox ();
+
+		} else {
+			DisableTextBox ();
+		}
+	}
+
+	void Update(){
+
+		if (!isActive) {
+			return;
+
+//			if (textBox.activeInHierarchy){
+				DisableTextBox();
+//			}
+		} else {
+			EnableTextBox ();
+		}
+
+		theText.text = textLines [currentLine];
+
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			currentLine++;
+		}
+
+		if (currentLine > endAtLine) {
+			DisableTextBox ();
+			isActive = false;
+		}
+	}
+
+
+
+	public void EnableTextBox(){
+		
+		textBox.SetActive(true);
+	}
+
+	public void DisableTextBox(){
+		
+		textBox.SetActive(false);
+	}
+}
