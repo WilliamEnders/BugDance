b2f840f181060b9002ae0e80ff4f09ec5dcb66cd
Claire Hu <hum085@newschool.edu>
2016-05-05 18:26:17 -0400

    no message

---
+++ Assets/bossTalking.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bossTalking : MonoBehaviour {
+
+	Animator anim;
+	bool isTalking = false;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Invoke ("talking", 0.01f);
+
+	
+	}
+
+	void talking(){
+		
+		anim.SetBool ("isTalking", true);
+
+	}
+}
