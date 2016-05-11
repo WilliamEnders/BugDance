cd625198c2a9fc2d2e4be84b600fc3547dbb1e8e
Claire Hu <hum085@newschool.edu>
2016-05-11 16:15:49 -0400

    no message

---
+++ Assets/beeAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class beeAnimation : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+
+		animator = GetComponent<Animator>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (GetComponent<beeTalking> ().showDance) {
+			animator.SetBool ("dance", true);
+		}
+	}
+
+}
