62743bf502885211aef3036b7545c8a5bea84629
William Enders <endew231@newschool.edu>
2016-05-11 15:53:23 -0400

    newspaper test

--- Assets/Scripts/checkPhotos.cs
+++ Assets/Scripts/checkPhotos.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class checkPhotos : MonoBehaviour {
 
 	public Transform[] inv;
+	public Transform[] imageFrames;
+	public GameObject newspaper;
 	
 
 	// Use this for initialization
@@ -13,10 +16,15 @@ public class checkPhotos : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(inv[0]!=null){
+			print ("wowow");
+			Texture2D textureSprite = inv [0].GetComponent<pictureInfo> ().texture;
+			imageFrames [0].GetComponent<Image> ().sprite = Sprite.Create(textureSprite, new Rect(0,0,textureSprite.width,textureSprite.height),new Vector2(0.5f,0.5f));
+		}
 	}
 
 	void OnTriggerEnter(Collider info){
+		newspaper.SetActive (true);
 		if(info.CompareTag("Player")){
 			for(int i=0; i < info.GetComponentInChildren<takePicture>().invNum; i++){
 				inv[i] = info.GetComponentInChildren<takePicture> ().inventory [i];
